35ae21e801b904ab35b01312b1367bb29d9f4073
Abdullah Khawer <abdullahkhawer@live.com>
2023-09-04 13:27:03 +0200

    Create wait_for_aws_ec2_instance_to_be_running_and_ready.tf

---
+++ terraform/wait_for_aws_ec2_instance_to_be_running_and_ready.tf
@@ -0,0 +1,32 @@
+# Terraform code to wait for an AWS EC2 instance to be running and complete status checks.
+# var.aws_ec2_instance_id needs to replaced or provided with the actual AWS EC2 instance id.
+
+resource "null_resource" "wait_for_instance" {
+  triggers = {
+    instance_id = var.aws_ec2_instance_id
+  }
+
+  provisioner "local-exec" {
+    command = <<EOF
+      timeout=600  # Timeout in seconds
+      start_time=$(date +%s)
+
+      until [ "$(aws ec2 describe-instance-status --instance-id ${self.triggers.instance_id} --query 'InstanceStatuses[0].InstanceStatus.Status' --output text)" = "ok" ] && \
+            [ "$(aws ec2 describe-instance-status --instance-id ${self.triggers.instance_id} --query 'InstanceStatuses[0].SystemStatus.Status' --output text)" = "ok" ] && \
+            [ "$(aws ec2 describe-instances --instance-ids ${self.triggers.instance_id} --query 'Reservations[0].Instances[0].State.Name' --output text)" = "running" ]; do
+        current_time=$(date +%s)
+        elapsed_time=$((current_time - start_time))
+
+        if [ "$elapsed_time" -ge "$timeout" ]; then
+          echo "Timeout reached. Instance didn't reach the desired state within $timeout seconds."
+          exit 1
+        fi
+
+        sleep 5
+        echo "Waiting for instance to be running and complete status checks..."
+      done
+
+      echo "Instance is now running fine and status checks have been completed!"
+    EOF
+  }
+}
